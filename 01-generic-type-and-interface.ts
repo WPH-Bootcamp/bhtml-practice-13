df96b66204064be5a3c4d79616255db3f3df52ea
Henry Rivardo <henrysihombing20@gmail.com>
2026-05-11 19:01:24 +0700

    typescript class 13

---
+++ 01-generic-type-and-interface.ts
@@ -0,0 +1,5 @@
+export { };
+
+console.log("==========================================");
+console.log("   BAGIAN 1: GENERIC TYPE & INTERFACE");
+console.log("==========================================\n");
