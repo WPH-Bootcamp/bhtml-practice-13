a46479a9ec829705775e978ab30c4d3dea5a5494
Henry Rivardo <henrysihombing20@gmail.com>
2026-05-11 20:44:51 +0700

    generic constraints

--- 02-generic-constraints.ts
+++ 02-generic-constraints.ts
@@ -1,2 +1,50 @@
 // kalian suatu tempat
 // kalian boleh bawa makanan apa saja ke tempat ini, asal tidak berbau menyengat
+
+// masalah tanpa syarat asal bebas
+function panjangGenerikSalah<T>(item: T) {
+  return item;
+}
+
+// console.log(panjangGenerikSalah("halo"));
+
+// harus kasih syarat
+
+interface PunyaLength {
+  length: number;
+}
+
+function ambilPanjang<T extends PunyaLength>(item: T): number {
+  return item.length;
+}
+
+// console.log(ambilPanjang("Hello world"));
+// console.log(ambilPanjang([10, 15, 20]));
+// console.log(ambilPanjang({ length: 100 }));
+
+// keyof constraint = ambil property object, pastikan key nya valid
+
+// k extends keyof T =
+// k hanya boleh berisi nama nama yang ada di T
+
+function ambilProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+const user = {
+  id: 1,
+  nama: "budi",
+  umur: 25,
+  email: "budi@gmail.com",
+  alamat: "Indonesia",
+};
+
+console.log(user["nama"]);
+
+console.log(user.alamat);
+
+console.log(ambilProperty(user, "nama"));
+
+console.log(ambilProperty(user, "umur"));
+console.log(ambilProperty(user, "email"));
+console.log(ambilProperty(user, "alamat"));
