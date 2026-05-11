f03246f0f8f67a3a6b8b3bce78dfd9cb87de32af
Henry Rivardo <henrysihombing20@gmail.com>
2026-05-11 22:06:25 +0700

    record

--- 05-mapped-types-records.ts
+++ 05-mapped-types-records.ts
@@ -23,3 +23,18 @@ const jadwal: TodoActivity = {
 };
 
 console.log(jadwal);
+
+type ThemeColor = "primary" | "secondary" | "success" | "danger" | "warning";
+
+interface ColorValue {
+  bg: string;
+  text: string;
+}
+
+const theme: Record<ThemeColor, ColorValue> = {
+  primary: { bg: "#3b82f6", text: "#ffffff" },
+  secondary: { bg: "#6b7280", text: "#ffffff" },
+  success: { bg: "#10b981", text: "#ffffff" },
+  danger: { bg: "#ef4444", text: "#ffffff" },
+  warning: { bg: "#f59e0b", text: "#000000" },
+};
