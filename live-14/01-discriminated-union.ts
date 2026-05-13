bb1972c7f524c61104e27855bd3e8e27eb88da24
Henry Rivardo <henrysihombing20@gmail.com>
2026-05-13 19:15:08 +0700

    class 14 typescript

---
+++ live-14/01-discriminated-union.ts
@@ -0,0 +1,5 @@
+export { };
+
+console.log("==========================================");
+console.log("   BAGIAN 1: DISCRIMINATED UNION");
+console.log("==========================================\n");
